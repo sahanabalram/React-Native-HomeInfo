212088a10a3b1003b2f0b1370e42c3e2159dd2b5
Sahana Balram <sahana2008balram@gmail.com>
2018-05-16 15:57:46 -0700

    Rewrite to display navigation

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation'
+import MainScreen from './Components/MainScreen'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <AppStackNavigator />
     );
   }
+
 }
+
+const AppStackNavigator = StackNavigator({
+
+  Main: {
+    screen: MainScreen
+  }
+})
+
 
 const styles = StyleSheet.create({
   container: {
