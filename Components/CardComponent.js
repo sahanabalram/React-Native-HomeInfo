601060f2d87d1df0ebcdde70b6351dc550d9093c
Sahana Balram <sahana2008balram@gmail.com>
2018-05-16 16:44:29 -0700

    Include photos and hard code weather information as a start

--- Components/CardComponent.js
+++ Components/CardComponent.js
@@ -1,14 +1,51 @@
 import React, {Component} from "react";
 import {StyleSheet, View, Text, Platform, Image} from "react-native";
 
-import {Icon,Card, CardItem, Thumbnail, Body, Left, Right} from "native-base";
+import {Icon,Card, CardItem, Thumbnail, Body, Left, Right, Button} from "native-base";
 
 class CardComponent extends Component{
     render(){
         return(
-            <View style={styles.container}>
-                <Text>Card Component</Text>
-            </View>
+            <Card>
+                <CardItem>
+                    <Left>
+                        <Thumbnail source={require("../assets/sahana.jpg")}/>
+                        <Body>
+                            <Text>Today's Weather</Text>
+                            <Text note>May 16, 2018</Text>
+                        </Body>
+                    </Left>
+                </CardItem>
+                <CardItem cardBody>
+                    <Thumbnail source={require("../assets/weather1.jpg")} style={{height:200, width:200,flex:1 }}/>
+                </CardItem>
+
+                <CardItem style={{height: 45}}>
+                    <Left>
+                        <Button transparent>
+                            <Icon name="ios-heart-outline" style={{color: "black"}}/>
+                        </Button>
+                        <Button transparent>
+                            <Icon name="ios-chatbubbles-outline" style={{color: "black"}}/>
+                        </Button>
+                        <Button transparent>
+                            <Icon name="ios-send-outline" style={{color: "black"}}/>
+                        </Button>
+                    </Left>
+                </CardItem>
+
+                <CardItem style={{height: 20}}>
+                    <Text>101</Text>
+                </CardItem>
+
+                <CardItem>
+                    <Body>
+                        <Text>
+                        <Text style={{fontWeight:"900"}}> sahana </Text>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</Text>
+                    </Body>
+                </CardItem>
+            </Card>
         )
     }
 }
